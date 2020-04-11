d0f1e3c891210523e47034294883bdbb801fad6d
Yesid A. López V <yesid4IT@gmail.com>
2020-04-11 15:03:11 +0000

    adding the main function

---
+++ main_shell.c
@@ -0,0 +1,46 @@
+#include "headershell.h"
+#include <stdio.h>
+
+
+/**
+ * main - Read, Search and execute the input
+ *
+ * Return: Always 0 success
+ */
+int main(int argc, char *argv[], char **env)
+{
+	(void)argc;
+	(void)argv;
+	char *line = NULL;
+	char **input; /* command typing by user */
+	char **binpath; /* pointer to PATH*/
+	int i, j;
+	size_t len = 0;
+	ssize_t read;/*chars read by getline() */
+	int exe_child; /*value return by execve*/
+	int status; /*status return*/
+
+	while (1)
+	{
+		printf("#cisfun$> ");/*prompt */
+
+/*// READING PHASE \\ */
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+			break;
+
+		input = get_input(line); /*receive the command type by the user */
+		printf("VERIFICANDO");
+
+		binpath = get_path(env);
+
+		i = 0, j = 0;
+		for (; input[i] != '\0'; i++)
+			printf("%s", input[i]);
+
+		for (; binpath[j] != '\0'; j++)
+                        printf("%s", binpath[j]);
+
+	}
+	return (0);
+}
