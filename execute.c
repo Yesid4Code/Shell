eab717cb2b842dedf0d46733c42146e811c4ee33
crispthoalex <alex877@gmail.com>
2020-04-12 06:35:20 +0000

    create execute function

---
+++ execute.c
@@ -0,0 +1,25 @@
+#include "headershell.h"
+
+/**
+ * execute - execute the path
+ * @binpath: string to execute + arguments
+ *
+ * Return: Always 0 success
+ */
+void execute(char **binpath)
+{
+	pid_t exechild;
+	int status;
+
+	exechild = fork();
+	if (exechild < 0)/*create a child procces? */
+		exit(EXIT_FAILURE);
+	else if (exechild == 0)
+	{
+		if (execve(*binpath, binpath, NULL) == -1)
+			exit(EXIT_FAILURE);
+	}
+	else
+		while (wait(&status) != pid)
+		{}
+}
