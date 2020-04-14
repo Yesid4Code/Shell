8a1b82292657da16fecafa27a5172417fe144a81
Yesid A. López V <yesid4IT@gmail.com>
2020-04-14 03:06:15 +0000

    adding funtion pathtoexecute

--- execute.c
+++ execute.c
@@ -1,5 +1,40 @@
 #include "headershell.h"
 
+/* SEARCHING PATH PHASE*/
+/**
+ * pathtoexecute - search directory of a command
+ * @argpath: receive the environ path.
+ *
+ * Return
+ */
+char *pathtoexecute(char **input)
+{
+	char **binpath;
+	char *fullpath = NULL;
+	int i = 0;
+	struct stat fpath;
+
+	binpath = get_path();
+	if (!binpath)
+	{
+		free(binpath), free(input);/*exit(EXIT_FAILURE);*/
+		return (NULL);
+	}
+	while (binpath[i] != NULL)
+	{       /*concatenate the strings*/
+		fullpath = _strdup(strcat(binpath[i], input[0]));
+		/*exists or can exe?*/
+		if ((stat(fullpath, &fpath) == 0))
+		{
+			free(binpath), free(input);
+			return (fullpath);
+		}
+		i++;
+	}
+	free(fullpath), free(binpath), free(input);
+	return (NULL);
+}
+
 /**
  * execute - execute the path
  * @binpath: string to execute + arguments
@@ -16,8 +51,11 @@ void execute(char **binpath)
 		exit(EXIT_FAILURE);
 	else if (exechild == 0)
 	{
-		if (execve(*binpath, binpath, NULL) == -1)
+		if (execve(*binpath, binpath, environ) == -1)
+		{
 			perror(*binpath), free(binpath);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 		while (wait(&status) != exechild)
