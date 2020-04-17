3fa0285c2d3241f9f9ca3187a980dc782c40b659
Yesid A. López V <yesid4IT@gmail.com>
2020-04-17 04:47:51 +0000

    adding the function _strncmp

--- headershell.h
+++ headershell.h
@@ -17,12 +17,13 @@ char **get_input();
 char **get_path();
 char *get_environ(char *envar);
 char *pathtoexecute(char **input);
-void execute(char **input);
+int execute(char **input);
 
 void control_c(int c);
 char *_strdup(char *str);
 int _strlen(char *s);
 int countstrings(char *str);
 char *str_concat(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
