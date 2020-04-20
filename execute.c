faee5108116ed3a50004bd83c6f65af8c3d4258f
crispthoalex <alex877@gmail.com>
2020-04-20 14:59:46 +0000

    remove comments

--- execute.c
+++ execute.c
@@ -1,39 +1,6 @@
 #include "headershell.h"
 
 /**
- * pathtoexecute - search directory of a command
- * @input: receive the environ path.
- *
- * Return: path to command, ex: /bin/ls
- *
-*char *pathtoexecute(char **input) SEARCHING PATH PHASE
-*{
-*	char **binpath = NULL;
-*	char *fullpath = NULL;
-*	int i = 0;
-*	struct stat fpath;
-*
-*	binpath = get_path();
-*	if (!binpath)
-*		return (NULL);
-*	while (binpath && binpath[i] != NULL) exists
-*	{concatenate the strings
-*		fullpath = str_concat(binpath[i], input[0]);
-*		exists or can exe?
-*		if ((stat(fullpath, &fpath) == 0))
-*		{
-*			freepptr(binpath), free(input[0]) MALLOC
-*			return (fullpath);
-*		}
-*		i++;
-*	}
-*	perror(*input);Print error message
-*	freepptr(binpath); ON'T FREE INPUT
-*	return (NULL);
-*}
-*/
-
-/**
  * execute - execute the path.
  * @input: string to execute + arguments.
  * @cmd: command path.
@@ -55,10 +22,6 @@ int execute(char *argv, char *fullpath,
 		exechild = fork();
 		if (exechild < 0) /*Not create a child procces */
 		{	/*PRINT VALUE FORK()*/
-			/*free(input);*/
-			/*sprintf(err_msg, "%s: %d: %s: not found\n",*/
-			/*argv[0], countexec, input[0]);*/
-			/*write(2, err_msg, _strlen(err_msg));*/
 			exit(2);
 			/*return (exechild);  exit(EXIT_FAILURE); */
 		}
@@ -75,8 +38,7 @@ int execute(char *argv, char *fullpath,
 			while (wait(&status) != exechild)
 			{}
 	}/* command not found. */
-	sprintf(error, "%s: %d: %s: not found\n",
-		argv, countexec, input[0]);
+	sprintf(error, "%s: %d: %s: not found\n", argv, countexec, input[0]);
 	write(2, error, _strlen(error));
 	/* free? */
 	return (-1);/* return (127) ?? */
