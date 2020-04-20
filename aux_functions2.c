bd116a6969a9ce2d40f24611efe1bbe1a2b59687
Yesid A. López V <yesid4IT@gmail.com>
2020-04-20 07:52:08 +0000

    Change the loop of the function freepptr

--- aux_functions2.c
+++ aux_functions2.c
@@ -28,6 +28,24 @@ int _strncmp(const char *s1, const char *s2, size_t n)
 }
 
 /**
+ * _copy - function that copy a string in other one.
+ * @dest: where src is going to be copied.
+ * @src: string to be copy.
+ *
+ * Return: pointer to dest.
+ */
+char *_copy(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	i = _strlen(src);
+	for (; j < i; j++)
+		dest[j] = src[j];
+	dest[j] = '\0';
+	return (dest);
+}
+
+/**
  * freepptr -  free each position and later the double pointer
  * @pptr: double pointer to free
  *
@@ -38,7 +56,39 @@ void freepptr(char **pptr)
 {
 	int i = 0;
 
-	for (i = 0; !pptr[i]; i++)
-		free(pptr[i]);
+	while (pptr && pptr[i])
+		free(pptr[i++]);
 	free(pptr);
+}
+
+/**
+ * _memseet
+ *
+ */
+char *_memset(char *str, char a, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		str[i] = a;
+	return (str);
+}
+
+/**
+ * _calloc - function that reserved memory and fill it with zeros.
+ * @sizeppter: number of strings inside the array line.
+ *
+ * Return: array filled with zeros.
+ */
+void *_calloc(unsigned int sizepptr, unsigned int sizetype)
+{
+	void *argline = NULL;
+
+	if (sizepptr == 0 || sizetype == 0)
+		return (NULL);
+	argline = malloc(sizepptr * sizetype + 1);
+	if (!argline)
+		return (NULL);
+	_memset(argline, 0, (sizepptr * sizetype));
+	return(argline);
 }
