3a021f4fe9a4bd95bef74358b2cc455128daf293
Yesid A. López V <yesid4IT@gmail.com>
2020-04-20 08:00:03 +0000

    adding _memset, _copy and _calloc functions

--- headershell.h
+++ headershell.h
@@ -15,10 +15,11 @@
 
 char *printpromt();
 char **get_input(char *line);
-char **get_path();
+char *get_path(char *input);
 char *get_environ(char *envar);
 char *pathtoexecute(char **input);
-int execute(char *cmd, char **input, char **env);
+int execute(char *argv, char *fullpath,
+	    char **input, char **env, unsigned int countexec);
 
 void control_c(int c);
 char *_strdup(char *str);
@@ -27,6 +28,8 @@ int countstrings(char *str);
 char *str_concat(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 void freepptr(char **pptr);
-
+void *_calloc(unsigned int sizepptr, unsigned int sizetype);
+char *_copy(char *dest, char *src);
+char *_memset(char *str, char a, unsigned int size);
 
 #endif /* END HEADERSHELL */
