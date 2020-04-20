98117e0575bc89f53b84f5aa32e39ae286fde8bd
Yesid A. López V <yesid4IT@gmail.com>
2020-04-20 07:55:26 +0000

    arguments received changed, print a new perro message

--- execute.c
+++ execute.c
@@ -5,57 +5,79 @@
  * @input: receive the environ path.
  *
  * Return: path to command, ex: /bin/ls
- */
-char *pathtoexecute(char **input)/* SEARCHING PATH PHASE*/
-{
-	char **binpath = NULL;
-	char *fullpath = NULL;
-	int i = 0, j = 0;
-	struct stat fpath;
-
-	binpath = get_path();
-	if (!binpath)
-		return (NULL);
-	while (binpath && binpath[i] != NULL)/* exists */
-	{/*concatenate the strings*/
-		fullpath = str_concat(binpath[i], input[0]);
-		/*exists or can exe?*/
-		if ((stat(fullpath, &fpath) == 0))
-		{
-			freepptr(binpath)/*, free(input[0])*/; /*DOBLE MALLOC - POSIBLE CONFLICTO*/
-			return (fullpath);
-		}
-		i++;
-	}
-	perror(*input);/*Print error message */
-	freepptr(binpath); /*DON'T FREE INPUT*/
-	return (NULL);
-}
+ *
+*char *pathtoexecute(char **input) SEARCHING PATH PHASE
+*{
+*	char **binpath = NULL;
+*	char *fullpath = NULL;
+*	int i = 0;
+*	struct stat fpath;
+*
+*	binpath = get_path();
+*	if (!binpath)
+*		return (NULL);
+*	while (binpath && binpath[i] != NULL) exists
+*	{concatenate the strings
+*		fullpath = str_concat(binpath[i], input[0]);
+*		exists or can exe?
+*		if ((stat(fullpath, &fpath) == 0))
+*		{
+*			freepptr(binpath), free(input[0]) MALLOC
+*			return (fullpath);
+*		}
+*		i++;
+*	}
+*	perror(*input);Print error message
+*	freepptr(binpath); ON'T FREE INPUT
+*	return (NULL);
+*}
+*/
 
 /**
- * execute - execute the path
- * @input: string to execute + arguments
- * @cmd: command path
- * @env: array of environment variables
- * Return: Always 0 success
+ * execute - execute the path.
+ * @input: string to execute + arguments.
+ * @cmd: command path.
+ * @env: array of environment variables.
+ * @countexec: number of times program executed.
+ *
+ * Return: -1 if error.
  */
-int execute(char *cmd, char **input, char **env)
+int execute(char *argv, char *fullpath,
+	    char **input, char **env, unsigned int countexec)
 {
-	pid_t exechild;
 	int status;
+	char error[100];
+	pid_t exechild;
+	struct stat fpath;
 
-	exechild = fork();
-	if (exechild < 0) /*Not create a child procces */
-	{	/*PRINT VALUE FORK()*/
-		/*free(input);*/
-		return (exechild); /* exit(EXIT_FAILURE); */
-	}
-	else if (exechild == 0)
+	if (stat(fullpath, &fpath) == 0)
 	{
-		return (execve(cmd, input, env));
-	}
-	else /* Successful forks return positive process id's the parent */
-		while (wait(&status) != exechild)
-		{}
-	return (0);
+		exechild = fork();
+		if (exechild < 0) /*Not create a child procces */
+		{	/*PRINT VALUE FORK()*/
+			/*free(input);*/
+			/*sprintf(err_msg, "%s: %d: %s: not found\n",*/
+			/*argv[0], countexec, input[0]);*/
+			/*write(2, err_msg, _strlen(err_msg));*/
+			exit(2);
+			/*return (exechild);  exit(EXIT_FAILURE); */
+		}
+		else if (exechild == 0)
+		{
+			if (execve(fullpath, input, env) == -1)
+			{
+				sprintf(error, "%s: %d: %s: not found\n",
+					argv, countexec, input[0]);
+				write(2, error, _strlen(error));
+			}
+		}
+		else /* Successful forks return positive process id's the parent */
+			while (wait(&status) != exechild)
+			{}
+	}/* command not found. */
+	sprintf(error, "%s: %d: %s: not found\n",
+		argv, countexec, input[0]);
+	write(2, error, _strlen(error));
+	/* free? */
+	return (-1);/* return (127) ?? */
 }
