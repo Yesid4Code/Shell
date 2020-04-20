77441c06c75380f62143a60b6d80ce05afc4058b
Yesid A. López V <yesid4IT@gmail.com>
2020-04-20 08:02:40 +0000

    Complete restructuring

--- main_shell.c
+++ main_shell.c
@@ -9,45 +9,39 @@
  * Return: Always 0 success
  */
 int main(int argc, char *argv[], char **env)
-{	/*https://pubs.opengroup.org/onlinepubs/009695399/functions/getcwd.html*/
-	char **input = NULL, *cmd = NULL; /* command & pathcommand */
-	int r = 0; /* check if the execute was successfull */
+{
+	char **input = NULL, *fullpath = NULL; /* command & pathcommand */
+	int countexec = 1; /* check if the execute was successfull */
 
-	(void)argc, (void)argv;
-	signal(SIGINT, control_c); /* read ctrl + C */
+	(void)argc, (void)argv, signal(SIGINT, control_c); /* read ctrl + C */
 	while (1)
 	{
-		input = get_input(printpromt());/* ||READING PHASE|| */
+		input = get_input(printpromt()); /* ||READING PHASE|| */
 		if (input == NULL)
-			continue; /*ADD executing in current directory getcwd()"./"??*/
-		if (**input != '/' || !strncmp(*input, "./", 2))
-		{/*||SEARCHING PATH PHASE || */
-			cmd = pathtoexecute(input);/*duplicate??*/
-			if (!cmd)
+		{
+			countexec++;
+			continue; /*ADD executing in getcwd() ?*/
+		}
+		if (**input != '/'/* || !strncmp(*input, "./", 2)*/)
+		{       /*||SEARCHING PATH PHASE || */
+			fullpath = get_path(*input);
+			if (fullpath) /* ||EXECUTE PHASE|| */
 			{
-				freepptr(input), free(cmd);/*free double pptr*/
-				continue;
-			}
-			else /* ||EXECUTE PHASE|| */
-			{
-				r = execute(cmd, input, env);
-				if (r == -1)
-				{
-					freepptr(input), free(cmd);
-					continue;
-				}
+				execute(argv[0], fullpath,
+					input, env, countexec);
 			}
 		}
 		else
 		{/**inpút = argv[0] = commandpath to execute*/
-			r = execute(*input, input, env);
-			if (r == -1)
-			{
-				freepptr(input);
-				continue;
-			}
+			execute(argv[0], *input, input, env, countexec);
+/*			if (r == -1)*/
+/*			{*/
+/*				freepptr(input);*/
+/*				continue;*/
+/*			}*/
 		}
 		freepptr(input);
+		countexec++;
 	}
 	return (0);
 }
