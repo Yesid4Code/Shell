b8c141c830123a628576c7eb9ff5d6906bce8407
Yesid A. López V <yesid4IT@gmail.com>
2020-04-21 01:37:56 +0000

    addint a exit status

--- get_functions.c
+++ get_functions.c
@@ -19,7 +19,7 @@ char *printpromt()
 		free(line);
 		if ((read == EOF) && isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "\n", 1);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_SUCCESS); /* EXIT_SUCCESS || 2 */
 	}
 	return (line);
 }
